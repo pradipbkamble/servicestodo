33773294fef7b7c8860b9cfa383d8d962a8fb177
pradipbkamble <pradipbkamble001@gmail.com>
2024-02-17 02:27:00 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodopssengerlistComponent } from './todopssengerlist/todopssengerlist.component';
+import { FormpassengerComponent } from './formpassenger/formpassenger.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodopssengerlistComponent,
+    FormpassengerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
